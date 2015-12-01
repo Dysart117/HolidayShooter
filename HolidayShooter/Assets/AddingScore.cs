8627ffd879dc3cfdc10d806afcd58c1199c67755
Jacob Dawson <jadawson@utc4me.org>
2015-12-01 10:18:40 -0500

    The magic of score
    jksadf

---
+++ HolidayShooter/Assets/AddingScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddingScore : MonoBehaviour
+{
+    [SerializeField]
+    ScoreManagement scoreManager;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            scoreManager.CurrentScore = scoreManager.CurrentScore + 10;
+        }
+    }
+}
