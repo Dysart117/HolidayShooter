4d337a977047b2051cb0f9614b4518e88b3d7345
Stanley H. Dystart II <ddysart@IT200.mr.preble.com>
2015-12-01 09:42:23 -0500

    Finish
    This finishes the guns.

--- HolidayShooter/Assets/GunTest/Scripts/Bullet.cs
+++ HolidayShooter/Assets/GunTest/Scripts/Bullet.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 
     //This script exists to control the bullet.
-	
     
-	void Start () {
-	
+    [SerializeField] float kBulletSpeed;    //This controls the speed of the bullet.
+
+	void Start ()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * kBulletSpeed);    //This propels the bullets forward.
+
+        Invoke("Shutdown", 3);  //This disables the bullet 3 seconds after it was made active.
 	}
 	
-	
-	void Update () {
-	
-	}
+	void Shutdown()
+    {   //This resets the velocity of the bullet, and disables it.
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+        gameObject.SetActive(false);
+    }
 }
