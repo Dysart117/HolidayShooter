1a79dc3cbda262f27b7f5cdae516255db1889969
Jacob Dawson <jadawson@utc4me.org>
2015-12-02 09:32:20 -0500

    most of day 2's work
    Day two

---
+++ HolidayShooter/Assets/Enemy Spawning/Scripts/RandomSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Enemy;
+
+    [SerializeField]
+    Transform[] SpawnPoints;
+
+    void Start()
+    {
+        int SpawnPointIndex = Random.Range(0, SpawnPoints.Length);
+
+        Instantiate(Enemy, SpawnPoints[SpawnPointIndex].position, SpawnPoints[SpawnPointIndex].rotation);
+    }
+}
