fac8927d9bbc087faef8d72dca52991cb8acda5b
Jacob Dawson <jadawson@utc4me.org>
2015-12-02 08:50:22 -0500

    level Design Stuff
    Trying again

---
+++ HolidayShooter/Assets/Level Design/ScoreManagement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManagement : MonoBehaviour
+{
+    [SerializeField]
+    Text ScoreText;
+
+    public int CurrentScore = 0;
+
+    void Start()
+    {
+        ScoreText.text = "Score: " + CurrentScore;
+    }
+
+    void Update()
+    {
+        ScoreText.text = "Score: " + CurrentScore;
+    }
+
+}
