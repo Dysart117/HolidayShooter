2cc304fb932ee3ca292d1b3f7c3cf51322f1457c
Stanley H. Dystart II <ddysart@IT200.mr.preble.com>
2015-12-04 10:22:49 -0500

    Animations

---
+++ HolidayShooter/Assets/Animation GINGY/Scripts/GingerEnemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GingerEnemy : MonoBehaviour {
+
+    //This script exists to manage the gingerbread people's animations.
+
+    Animator m_Animator;
+
+    void Start ()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Test1");
+        if (other.tag == "Player")
+        {
+            Debug.Log("Test2");
+            m_Animator.SetBool("IsPunching", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_Animator.SetBool("IsPunching", true);
+        }
+    }
+
+    public void Dead()
+    {
+        m_Animator.SetBool("IsDead", true);
+    }
+}
