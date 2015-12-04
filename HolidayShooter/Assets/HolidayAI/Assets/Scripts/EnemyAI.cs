6b21b94dcfd33569540180015712e8ad2d4a4fc8
Alex Rediker <arediker@IT400.mr.preble.com>
2015-12-04 08:59:11 -0500

    Holiday AI, Holiday GUN and Nav Mesh at our disposal!
    Has all of the three things previously mentioned

---
+++ HolidayShooter/Assets/HolidayAI/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour 
+{
+	public Transform player;
+	public float playerDistance;
+	public float rotationDamping;
+	public float moveSpeed;
+	
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () {
+		
+		//if(HealthScrip01.isPlayerAlive)
+		
+		playerDistance = Vector3.Distance (player.position, transform.position);
+		
+		if (playerDistance < 25f) 
+		{
+			lookAtPlayer ();
+		}
+		if (playerDistance < 25f) 
+		{
+			if(playerDistance > 2f)
+				
+			{
+				chase ();
+			}
+			else if(playerDistance < 2f)
+			{
+				attack();
+			}
+		}
+	}
+	void lookAtPlayer()
+	{
+		Quaternion rotation = Quaternion.LookRotation (player.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationDamping);
+	}
+	
+	void chase()
+	{
+		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+	void attack()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast (transform.position, transform.forward, out hit))
+		{
+			
+			if(hit.collider.gameObject.tag == "Player")
+			{
+
+			}
+		}
+	}	
+}
+
+
+
---
+++ HolidayShooter/Assets/HolidayAI/Assets/Scripts/NavMesh.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMesh : MonoBehaviour 
+{
+	private NavMeshAgent Internal_Nav_Mesh_Agent;
+
+	public GameObject Object_to_walk_to;
+
+	void Start () 
+	{
+		Internal_Nav_Mesh_Agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update () 
+	{
+		Internal_Nav_Mesh_Agent.SetDestination (Object_to_walk_to.transform.position);
+	}
+}
